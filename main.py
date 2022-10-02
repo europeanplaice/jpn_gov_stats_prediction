918753e3e73515cafa66f672d482f5c4ced21f15
europeanplaice <paul.thomas.goodfellas@gmail.com>
2022-10-02 23:01:21 +0900

    update main.py

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ from scipy.stats import zscore
 from darts.utils.likelihood_models import GaussianLikelihood
 from tqdm import tqdm
 import copy
+from darts.metrics.metrics import smape
+import os, shutil
 
 
 def old_data():
@@ -20,9 +22,7 @@ def old_data():
         l.append(tmp)
     df = pd.concat(l, axis=1)
     df.index = list(
-        map(
-            lambda x: datetime.datetime(int(str(x)[:4]), int(str(x)[4:]), 1), df.index
-        )
+        map(lambda x: datetime.datetime(int(str(x)[:4]), int(str(x)[4:]), 1), df.index)
     )
     df = df.reset_index(names="time")
     return df
@@ -68,7 +68,6 @@ def prepare_timeseries(df: pd.DataFrame, category: list[str], split: bool):
         except (TypeError, ValueError):
             print("error", i)
             continue
-        # df[i] = (df[i] - df[i].mean()) / df[i].std()
         series = TimeSeries.from_dataframe(df, time_col="time", value_cols=i)
         if split:
             train, val = series.split_before(pd.Timestamp("2021-01-01"))
@@ -86,47 +85,56 @@ def main():
     category_old = df_old.columns[df_old.columns != "time"]
     df = latest_data()
     category = df.columns[df.columns != "time"]
-    assert len(category) == len(list(set(category))), set([x for x in category if category.count(x) > 1])
-    # model_checkpoint = ModelCheckpoint(monitor="val_loss")
-    pl_trainer_kwargs = {"callbacks": [EarlyStopping("val_loss", patience=3)]}
-    n_epochs_pre = 2
-    n_epochs = 4
+    assert len(category) == len(list(set(category))), set(
+        [x for x in category if category.count(x) > 1]
+    )
+    n_epochs_pre = 1
+    n_epochs = 3
+    n_epochs_finetune = 1
     group_pre, _, _ = prepare_timeseries(df_old, category_old, split=False)
     group, train_group, val_group = prepare_timeseries(df, category, split=True)
     model = NBEATSModel(
         input_chunk_length=12,
         output_chunk_length=6,
-        # pl_trainer_kwargs=pl_trainer_kwargs,
         save_checkpoints=True,
-        model_name="a",
+        model_name="pretrain",
         force_reset=True,
         n_epochs=0,
         batch_size=1024,
-        likelihood=GaussianLikelihood()
-        # dropout=0.2,
-        # optimizer_kwargs={"lr": 1e-4},
+        likelihood=GaussianLikelihood(),
+        num_blocks=1
     )
     model.fit(group_pre, verbose=True, epochs=n_epochs_pre)
-    model = NBEATSModel.load_from_checkpoint("a", best=False)
-    model.fit(train_group, val_series=val_group, verbose=True, epochs=n_epochs_pre + n_epochs)
-    prediction_on_val = model.predict(len(val_group[0]), series=train_group, num_samples=100)
-    prediction_on_val.to_pickle("prediction_on_val.pickle")
-    model = NBEATSModel.load_from_checkpoint("a", best=False)
-    model.fit(group, verbose=True, epochs=n_epochs_pre + n_epochs + 2)
-    future_prediction = model.predict(18, series=group, num_samples=100)
-    future_prediction.to_pickle("future_prediction.pickle")
+    shutil.copytree("darts_logs/pretrain", "darts_logs/train", dirs_exist_ok=True)
+    model = NBEATSModel.load_from_checkpoint("train", best=False)
+    model.fit(
+        train_group, val_series=val_group, verbose=True, epochs=n_epochs_pre + n_epochs
+    )
+    shutil.copytree("darts_logs/train", "darts_logs/finetuning", dirs_exist_ok=True)
+    prediction_on_val = model.predict(
+        len(val_group[0]), series=train_group, num_samples=100, n_jobs=-1
+    )
+    model = NBEATSModel.load_from_checkpoint("finetuning", best=False)
+    model.fit(group, verbose=True, epochs=n_epochs_pre + n_epochs + n_epochs_finetune)
+    future_prediction = model.predict(18, series=group, num_samples=100, n_jobs=-1)
+    smape_val: list[float] = []
     for i in tqdm(range(len(val_group))):
+        smape_val.append(smape(val_group[i], prediction_on_val[i]))
         plt.figure(figsize=(12, 6))
         group[i].plot(label="actual")
-        # import pdb; pdb.set_trace()
-        # prediction[i].pd_series().plot(label="forecast", lw=3)
         prediction_on_val[i].plot(label="prediction as validation")
+        pd.concat(
+            [
+                prediction_on_val[i].quantiles_df(),
+                future_prediction[i].quantiles_df()
+            ]).to_excel(f"sheets/{category[i]}.xlsx")
         future_prediction[i].plot(label="future prediction")
         plt.legend()
-        plt.savefig(f"data/high/{category[i]}.jpg", dpi=300)
-        plt.savefig(f"data/low/{category[i]}.jpg", dpi=72)
+        plt.savefig(f"data/high_quality/{category[i]}.jpg", dpi=300)
+        plt.savefig(f"data/low_quality/{category[i]}.jpg", dpi=72)
         plt.close()
-        # plt.show()
+    pd.DataFrame({"cat": category, "smape": smape_val}).to_excel("stats.xlsx")
+    # plt.show()
 
 
 if __name__ == "__main__":
